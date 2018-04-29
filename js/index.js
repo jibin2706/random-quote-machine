7f98cb596c57442b6db84c9b85445ca83632fdbb
Jibin Thomas <jibin.thomas2706@gmail.com>
2018-04-29 16:21:36 +0530

    Quote Displaying on HTML

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 // API URL
 const url = 'https://talaikis.com/api/quotes/random/';
 
+// Selecting DOM Elements
 const button = document.querySelector('#quote__generate');
+const parent = document.getElementById('quote__display');
+let quote = document.createElement('p');
+let author = document.createElement('p');
 
-
+// Generate new quote on Click Event
 button.addEventListener('click', () => {
 	const data = fetch(url).then((request) => request.json()).then((data) => {
-			const quote = data.quote;
-			const author = data.author;
 			
-			// console.log(quote,author);
+			quote.innerHTML = data.quote;
+			author.innerHTML = `- ${data.author}`;
+
+			parent.appendChild(quote);
+			parent.appendChild(author);
 		});
 });
