a49eca66523aae636385b252252e2339ae59eb2e
Jibin Thomas <jibin.thomas2706@gmail.com>
2018-04-29 18:33:58 +0530

    Created and styled the webpage

--- js/index.js
+++ js/index.js
@@ -4,17 +4,36 @@ const url = 'https://talaikis.com/api/quotes/random/';
 // Selecting DOM Elements
 const button = document.querySelector('#quote__generate');
 const parent = document.getElementById('quote__display');
+const tweet = document.getElementsByClassName('twitter-share-button')[0];
 let quote = document.createElement('p');
 let author = document.createElement('p');
 
-// Generate new quote on Click Event
-button.addEventListener('click', () => {
-	const data = fetch(url).then((request) => request.json()).then((data) => {
-			
+// Quote Generater
+const quoteGenerate = () => {
+	const data = fetch(url).then((request) => request.json()).
+		then((data) => {		
 			quote.innerHTML = data.quote;
 			author.innerHTML = `- ${data.author}`;
+			tweet.href=`https://twitter.com/intent/tweet?text=${data.quote}-${data.author}`;
 
 			parent.appendChild(quote);
-			parent.appendChild(author);
-		});
-});
+			parent.appendChild(author);		
+			randomColorGenerater();
+	});
+};
+
+
+// Generate new quote on Click Event
+button.addEventListener('click', quoteGenerate);
+window.onload = quoteGenerate;
+
+
+// Random background color
+const randomNumber = no => Math.floor(Math.random() * no);
+
+const randomColorGenerater = () => {
+	const color = ['#D50000','#4A148C','#0D47A1','#009688','#E65100','#212121','#263238','#1B5E20','#01579B','#3F51B5','#AA00FF','#880E4F','#B71C1C'];
+	document.body.style.background = color[randomNumber(color.length)];
+}
+
+
